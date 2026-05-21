23d85be65b471e4b034201564dac4e0089bd1a4d
Oscar Ivan <oilp.4022@gmail.com>
2026-05-20 20:14:25 -0600

    feat(catalogo): implementacion de galeria de imagenes para la nueva coleccion de verano

--- Js/api.js
+++ Js/api.js
@@ -1,7 +1,7 @@
 import { CacheSystem } from './cache.js';
 
 export class API {
-    static BASE_URL = 'https://onach-urban-store.netlify.app';
+    static BASE_URL = 'https://onach-api.onrender.com/api';
 
     static getToken() {
         return localStorage.getItem('onachStoreToken');
